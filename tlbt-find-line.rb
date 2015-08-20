17c218248394437db829d50b3173695b30e47e21
Philipp Brendel <waldrumpus@gmail.com>
2015-08-20 12:33:51 +0200

    fix: beginning to fix find line script

--- tlbt-find-line.rb
+++ tlbt-find-line.rb
@@ -13,14 +13,17 @@ if ARGV.length < 2
 end
 
 game_script_file = ARGV[0]
-seach_regexp = Regexp.compile(ARGV[1])
+pattern = ARGV[1]
+encoding_name = "UTF-16LE"
+seach_regexp = Regexp.compile(pattern.encode(encoding_name))
 
 unless File.exists?(game_script_file)
   puts "file #{game_script_file} does not exist"
   exit(1)
 end
             
-script = File.open(game_script_file).read
+script = File.open(game_script_file, "rb:#{encoding_name}").read
+script_lines = [script]
 
 last_page_lines = [1000, 
                    1999, 
@@ -34,13 +37,13 @@ last_page_lines = [1000,
                    12142,
                    13043]
 
-script.lines.each_with_index do |line, line_index|
+script_lines.each_with_index do |line, line_index|
   next unless seach_regexp.match line
   
   page_number = last_page_lines.find_index {|ll| line_index < ll}
   
   if page_number
-    puts "#{line_index}  #{page_number + 1}: #{line}"
+    puts "#{line_index}  #{page_number + 1}: #{line.encode('UTF-8')}"
   else
     puts "error: invalid page number for line #{line_index}"
   end
