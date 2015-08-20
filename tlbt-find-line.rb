a61eb53ad53c6b15513a074216908af385dcba42
Philipp Brendel <waldrumpus@gmail.com>
2015-08-20 14:55:27 +0200

    fix: changed encoding of regexp to UTF-16LE

--- tlbt-find-line.rb
+++ tlbt-find-line.rb
@@ -14,6 +14,7 @@ end
 
 game_script_file = ARGV[0]
 pattern = ARGV[1]
+
 encoding_name = "UTF-16LE"
 seach_regexp = Regexp.compile(pattern.encode(encoding_name))
 
@@ -22,8 +23,7 @@ unless File.exists?(game_script_file)
   exit(1)
 end
             
-script = File.open(game_script_file, "rb:#{encoding_name}").read
-script_lines = [script]
+script = File.open(game_script_file, "r").read.force_encoding(encoding_name)
 
 last_page_lines = [1000, 
                    1999, 
@@ -37,13 +37,15 @@ last_page_lines = [1000,
                    12142,
                    13043]
 
-script_lines.each_with_index do |line, line_index|
+script.lines.each_with_index do |line, line_index|  
   next unless seach_regexp.match line
   
   page_number = last_page_lines.find_index {|ll| line_index < ll}
   
   if page_number
-    puts "#{line_index}  #{page_number + 1}: #{line.encode('UTF-8')}"
+    page_start_offset = (page_number > 0) ? (last_page_lines[page_number - 1]) : 0
+    page_line_index = line_index - page_start_offset
+    puts "#{line_index}  #{page_number + 1}:#{page_line_index + 1}  #{line.encode('UTF-8')}"
   else
     puts "error: invalid page number for line #{line_index}"
   end
